5c1d16621846c3859b0b6a87bf94242ce03b86d9
Gabriel Barbosa <gabrielbarbosadorio@gmail.com>
2023-08-14 12:04:37 -0300

    Esqueleto do Hero, CustomButton e Navbar feito

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+"use client"
+
+import Image from "next/image";
+import { CustomButton } from ".";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+  const handleScroll = () => {};
 
-export default Hero
+  return (
+    <div className="hero">
+
+      <div className="flex-1 pt-36 paddind-x">
+        <h1 className="hero__title">
+          Encontre, reserve ou alugue um carro - de forma rápida e fácil
+        </h1>
+
+        <p className="hero__subtitle">
+          Simplifique sua experiência de aluguel de carros com nosso processo de
+          reserva sem esforço.
+        </p>
+
+        <CustomButton
+          title="Explorar carros"
+          btnType="button"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain"/>
+
+          <div className="hero__image-overlay"/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
